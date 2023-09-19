06ca4a6a508813271467cff64feb5a244d8e84a5
Henry Anyaele Newman <anyaelenh09451@gmail.com>
2023-09-20 00:58:08 +0100

    MedProSearch

--- comments.php
+++ comments.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * @Packge     : Medixi
- * @Version    : 1.0
- * @Author     : Vecurosoft
- * @Author URI : https://www.templatemonster.com/authors/vecuro/
- *
- */
+
 
     // Block direct access
     if( ! defined( 'ABSPATH' ) ){
@@ -22,7 +16,7 @@
 <!-- Comments -->
 <div class="vs-comment-area vs-comments-layout1">
     <h3 class="h2 inner-title1 mb-lg-4 pb-lg-2">
-        <?php printf( _nx( '1 Comment', '%1$s Comments', get_comments_number(), 'comments title', 'medixi' ), number_format_i18n( get_comments_number() ) ); ?>
+        <?php printf( _nx( '1 Comment', '%1$s Comments', get_comments_number(), 'comments title', 'medprosearch' ), number_format_i18n( get_comments_number() ) ); ?>
     </h3>
     <ul class="comment-list">
         <?php
@@ -31,7 +25,7 @@
                     'style'       => 'ul',
                     'short_ping'  => true,
                     'avatar_size' => 100,
-                    'callback'    => 'medixi_comment_callback'
+                    'callback'    => 'medprosearch_comment_callback'
                 ) );
             the_comments_navigation();
         ?>
@@ -49,7 +43,7 @@
     $aria_req = ( $req ? "required" : '' );
     $consent = empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"';
 	$fields =  array(
-	  'author'  => '<div class="row"><div class="col-md-6 form-group"><input class="form-control style3" type="text" name="author" placeholder="'. esc_attr__( 'Enter Your Name *', 'medixi' ) .'" value="'. esc_attr( $commenter['comment_author'] ).'" '.esc_attr( $aria_req ).'></div>',
+	  'author'  => '<div class="row"><div class="col-md-6 form-group"><input class="form-control style3" type="text" name="author" placeholder="'. esc_attr__( 'Enter Your Name *', 'medprosearch' ) .'" value="'. esc_attr( $commenter['comment_author'] ).'" '.esc_attr( $aria_req ).'></div>',
 	  'email'   => '<div class="col-md-6 form-group"><input class="form-control style3" type="email" name="email"  value="' . esc_attr(  $commenter['comment_author_email'] ) .'" placeholder="'. esc_attr__( 'Enter E-mail Address *', 'medixi' ) .'" '.esc_attr( $aria_req ).'></div></div>',
       'url'     => '',
       'cookies' => '<div class="row"><div class="col-12"><div class="custom-checkbox notice mb-30"><input id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . esc_attr( $consent ) . ' />' . '<label for="wp-comment-cookies-consent">'  . esc_html__( ' Save my name, email, and website in this browser for the next time I comment.','medixi' ) .  '<span class="checkmark"></span> </label> </div></div></div>'
@@ -62,10 +56,10 @@
     	'title_reply_before'    => '<div class="form-title"><h3 class="h2">',
         'title_reply_after'     => '</h3></div>',
         'comment_notes_before'  => '<p class="form-text">'.esc_html__('Your email address will not be published. Required fields are marked *','medixi').'</p>',
-        'logged_in_as'          => '<p class="logged-in-as mb-4 pb-2">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>','medixi' ), admin_url( 'profile.php' ), esc_attr( $user_identity ), wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>',
+        'logged_in_as'          => '<p class="logged-in-as mb-4 pb-2">' . sprintf( __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>','medprosearch' ), admin_url( 'profile.php' ), esc_attr( $user_identity ), wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) ) ) . '</p>',
         'class_submit'          => 'vs-btn style2',
         'submit_field'          => '<div class="row"><div class="col-12 form-group pt-xl-20 mb-0">%1$s %2$s</div></div>',
-    	'submit_button'         => '<button type="submit" name="%1$s" id="%2$s" class="%3$s">'.esc_html__('Post Comment','medixi').'<i class="fas fa-chevron-right"></i></button>',
+    	'submit_button'         => '<button type="submit" name="%1$s" id="%2$s" class="%3$s">'.esc_html__('Post Comment','medprosearch').'<i class="fas fa-chevron-right"></i></button>',
     	'fields'                => $fields,
 	);
 
